324f7425f6426eae5cb197de1cd2dfd146104e1c
Christian Guillermo Avila Camberos <chguavilaca@ittepic.edu.mx>
2025-04-03 00:36:27 -0700

    clase

---
+++ src/main/java/ca/basededatos/BaseDeDatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.basededatos;
+
+/**
+ *
+ * @author guill
+ */
+public class BaseDeDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/ca/basededatos/articulo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.basededatos;
+
+/**
+ *
+ * @author guill
+ */
+public class articulo {
+    private String  clave;
+    private String descripcion;
+    private float precio;
+    private int cantidad;
+
+    public articulo(String clave, String descripcion, float precio, int cantidad) {
+        this.clave = clave;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+    
+    public void insertar(){
+    
+    }
+}//clase
